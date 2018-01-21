4a65529953b944cd2c9ae79832bed4bb1dbc114a
peter279k <peter279k@gmail.com>
2018-01-21 21:25:04 +0800

    add tests code

---
+++ tests/ImageTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Odan\Image\Test;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * ImageTest Class
+ */
+class ImageTest extends TestCase
+{
+    public function convertImageTest()
+    {
+        
+    }
+}
